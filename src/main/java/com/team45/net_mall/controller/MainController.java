acfbaea714f43178662196a16aefa91d134cfc8b
xseven <niko.k@qq.com>
2020-07-27 17:17:28 +0800

    在MainController中添加进入后台的方法

--- src/main/java/com/team45/net_mall/controller/MainController.java
+++ src/main/java/com/team45/net_mall/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.team45.net_mall.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
@@ -11,5 +12,9 @@ public class MainController {
         return "/front-end/index.html";
     }
      //这是dev1开发线的测试
+    @GetMapping("/index")
+    public String toAdminlte(){
+        return "/index.html";
+    }
 
 }

262dd773ac234ec0a283228e1e5e617f1d5f8148
胡金宝 <niko.k@qq.com>
2020-07-30 23:17:33 +0800

    Update NetMallApplicationTests.java 注释掉方法

--- src/test/java/com/team45/net_mall/NetMallApplicationTests.java
+++ src/test/java/com/team45/net_mall/NetMallApplicationTests.java
@@ -1,13 +1,13 @@
-package com.team45.net_mall;
-
-import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-
-//@SpringBootTest
-class NetMallApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.team45.net_mall;
+//
+//import org.junit.jupiter.api.Test;
+////import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class NetMallApplicationTests {
+//
+//    //@Test
+//    //void contextLoads() {
+//    //}
+//
+//}
